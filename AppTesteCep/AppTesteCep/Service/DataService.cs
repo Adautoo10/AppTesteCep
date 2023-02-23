6e109ce08ee6caaadb9cb45b511e609d188adf1f
Usuario <Usuario@CPD02-M04>
2023-02-23 15:07:38 -0300

    Atualizando Projeto

--- AppTesteCep/AppTesteCep/Service/DataService.cs
+++ AppTesteCep/AppTesteCep/Service/DataService.cs
@@ -37,7 +37,7 @@ namespace AppTesteCep.Service
         {
             List<Bairro> arr_bairros = new List<Bairro>();
 
-            using (HttpClient client = new HttpClient()) 
+            using (HttpClient client = new HttpClient())
             {
                 HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/bairro/by-cidade?id_cidade=");
                 if (response.IsSuccessStatusCode)
@@ -52,5 +52,30 @@ namespace AppTesteCep.Service
 
             return arr_bairros;
         }
+
+        public static async Task<List<Cidade>> GetCidadesByEstado(string uf)
+        {
+            List<Cidade> arr_cidades = new List<Cidade>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/cidade/by-uf?uf=");
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    arr_cidades = JsonConvert.DeserializeObject<List<Cidade>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return arr_cidades;
+        }
+
     }
+
+
 }
+
+
--- AppTesteCep/AppTesteCep/View/BairrosPorCidade.xaml.cs
+++ AppTesteCep/AppTesteCep/View/BairrosPorCidade.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppTesteCep.Model;
+using AppTesteCep.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,45 @@ namespace AppTesteCep.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BairrosPorCidade : ContentPage
     {
+        ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+
+        ObservableCollection<Bairro> lista_bairros = new ObservableCollection<Bairro>();
+
         public BairrosPorCidade()
         {
             InitializeComponent();
+
+            pck_cidade.ItemsSource = lista_cidades;
+
+            lst_bairros.ItemsSource = lista_bairros;
+        }
+
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Picker disparador = sender as Picker;
+
+                string estado_selecionado = disparador.SelectedItem as string;
+
+                List<Cidade> arr_cidades = await DataService.GetCidadesByEstado(estado_selecionado);
+
+                lista_cidades.Clear();
+
+                arr_cidades.ForEach(i => lista_cidades.Add(i));
+            }
+
+            catch (Exception ex) 
+            { 
+            await DisplayAlert("Ops", ex.Message, "OK");
+            }
+            
+
+        }
+
+        private void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
