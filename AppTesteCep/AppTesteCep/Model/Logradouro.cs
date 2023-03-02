b7af5db0e30de208570bd83d839b74e115b4b1b0
Usuario <Usuario@CPD02-M04>
2023-03-02 15:05:18 -0300

    Atualizando

---
+++ AppTesteCep/AppTesteCep/Model/Logradouro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTesteCep.Model
+{
+    public class Logradouro
+    {
+        public int id_logradouro { get; set; }
+        public string descricao { get; set; }
+        public string tipo { get; set; }
+        public int cep { get; set; }
+        
+    }
+}
