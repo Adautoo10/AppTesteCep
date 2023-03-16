9f8034c064de08bd54edd40da5afa26f74431f72
Usuario <Usuario@CPD02-M04>
2023-03-16 14:21:40 -0300

    Atualizando parte de cep e busca de cep

--- AppTesteCep/AppTesteCep/Model/Cep.cs
+++ AppTesteCep/AppTesteCep/Model/Cep.cs
@@ -6,5 +6,16 @@ namespace AppTesteCep.Model
 {
     public class Cep
     {
+        public string CEP { get; set; }
+        public string id_logradouro { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+        public string id_cidade { get; set; }
+        public string UF { get; set; }
+        public string complemento { get; set; }
+        public string descricao_sem_numero { get; set; }
+        public string descricao_cidade { get; set; }
+        public string codigo_cidade_ibge { get; set; }
+        public string descricao_bairro { get; set; }
     }
 }
